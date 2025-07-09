8ea91459653ef99aff4fc36426860ac37dcf9e97
Samia Maqsood <samiamaqsoood@gmail.com>
2025-07-09 17:40:16 +0500

    edited package.json

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,10 @@ import './HomeText.css'
 export default function Home() {
   return (
     <div className='bg-robo'>
-      <Spline className='robo' scene="/models/robo_model.spline" />
+      {/* <Spline className='robo' scene="/models/robo_model.spline" /> */}
+        <Spline className='robo' scene="/Portfolio-SamiaMaqsood/models/robo_model.spline  " />
+      {/* /Portfolio-SamiaMaqsood/models/robo_model.spline */}
+  
        <HomeTextCall />
     </div>
   );
